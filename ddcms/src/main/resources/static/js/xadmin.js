d37e6d8ee543ad45a78ff57eae59878ece73fe4e
周峰 <zhoufengjob@sina.com>
2017-12-22 11:45:21 +0800

    优化弹窗显示

--- ddcms/src/main/resources/static/js/xadmin.js
+++ ddcms/src/main/resources/static/js/xadmin.js
@@ -1,62 +1,63 @@
 $(function () {
     //加载弹出层
-    layui.use(['form','element'],
-    function() {
-        layer = layui.layer;
-        element = layui.element;
+    layui.use(['form', 'element'],
+        function () {
+            layer = layui.layer;
+            element = layui.element;
 
 
-        tab.tabAdd("仪表盘","/admin/main",4);
-        tab.tabChange(4);
-    });
+            tab.tabAdd("仪表盘", "/admin/main", 4);
+            tab.tabChange(4);
+        });
 
 
     //触发事件
-  var tab = {
-        tabAdd: function(title,url,id){
-          //新增一个Tab项
-          element.tabAdd('xbs_tab', {
-            title: title 
-            ,content: '<iframe tab-id="'+id+'" frameborder="0" src="'+url+'" scrolling="yes" class="x-iframe"></iframe>'
-            ,id: id
-          })
+    var tab = {
+        tabAdd: function (title, url, id) {
+            //新增一个Tab项
+            element.tabAdd('xbs_tab', {
+                title: title
+                ,
+                content: '<iframe tab-id="' + id + '" frameborder="0" src="' + url + '" scrolling="yes" class="x-iframe"></iframe>'
+                ,
+                id: id
+            })
         }
-        ,tabDelete: function(othis){
-          //删除指定Tab项
-          element.tabDelete('xbs_tab', '44'); //删除：“商品管理”
-          
-          
-          othis.addClass('layui-btn-disabled');
-        }
-        ,tabChange: function(id){
-          //切换到指定Tab项
-          element.tabChange('xbs_tab', id); //切换到：用户管理
-        }
-      };
+        , tabDelete: function (othis) {
+            //删除指定Tab项
+            element.tabDelete('xbs_tab', '44'); //删除：“商品管理”
 
+
+            othis.addClass('layui-btn-disabled');
+        }
+        , tabChange: function (id) {
+            //切换到指定Tab项
+            element.tabChange('xbs_tab', id); //切换到：用户管理
+        }
+    };
 
 
     tableCheck = {
-        init:function  () {
-            $(".layui-form-checkbox").click(function(event) {
-                if($(this).hasClass('layui-form-checked')){
+        init: function () {
+            $(".layui-form-checkbox").click(function (event) {
+                if ($(this).hasClass('layui-form-checked')) {
                     $(this).removeClass('layui-form-checked');
-                    if($(this).hasClass('header')){
+                    if ($(this).hasClass('header')) {
                         $(".layui-form-checkbox").removeClass('layui-form-checked');
                     }
-                }else{
+                } else {
                     $(this).addClass('layui-form-checked');
-                    if($(this).hasClass('header')){
+                    if ($(this).hasClass('header')) {
                         $(".layui-form-checkbox").addClass('layui-form-checked');
                     }
                 }
-                
+
             });
         },
-        getData:function  () {
+        getData: function () {
             var obj = $(".layui-form-checked").not('.header');
-            var arr=[];
-            obj.each(function(index, el) {
+            var arr = [];
+            obj.each(function (index, el) {
                 arr.push(obj.eq(index).attr('data-id'));
             });
             return arr;
@@ -65,30 +66,30 @@ $(function () {
 
     //开启表格多选
     tableCheck.init();
-      
 
-    $('.container .left_open i').click(function(event) {
-        if($('.left-nav').css('left')=='0px'){
+
+    $('.container .left_open i').click(function (event) {
+        if ($('.left-nav').css('left') == '0px') {
             $('.left-nav').animate({left: '-221px'}, 100);
             $('.page-content').animate({left: '0px'}, 100);
             $('.page-content-bg').hide();
-        }else{
+        } else {
             $('.left-nav').animate({left: '0px'}, 100);
             $('.page-content').animate({left: '221px'}, 100);
-            if($(window).width()<768){
+            if ($(window).width() < 768) {
                 $('.page-content-bg').show();
             }
         }
 
     });
 
-    $('.page-content-bg').click(function(event) {
+    $('.page-content-bg').click(function (event) {
         $('.left-nav').animate({left: '-221px'}, 100);
         $('.page-content').animate({left: '0px'}, 100);
         $(this).hide();
     });
 
-    $('.layui-tab-close').click(function(event) {
+    $('.layui-tab-close').click(function (event) {
         $('.layui-tab-title li').eq(0).find('i').remove();
     });
 
@@ -97,13 +98,13 @@ $(function () {
     // $('#content').bind("click",function(event){
     $('.left-nav #nav li').click(function (event) {
 
-        if($(this).children('.sub-menu').length){
-            if($(this).hasClass('open')){
+        if ($(this).children('.sub-menu').length) {
+            if ($(this).hasClass('open')) {
                 $(this).removeClass('open');
                 $(this).find('.nav_right').html('&#xe697;');
                 $(this).children('.sub-menu').stop().slideUp();
                 $(this).siblings().children('.sub-menu').slideUp();
-            }else{
+            } else {
                 $(this).addClass('open');
                 $(this).children('a').find('.nav_right').html('&#xe6a6;');
                 $(this).children('.sub-menu').stop().slideDown();
@@ -111,26 +112,27 @@ $(function () {
                 $(this).siblings().find('.nav_right').html('&#xe697;');
                 $(this).siblings().removeClass('open');
             }
-        }else{
+        } else {
 
             var url = $(this).children('a').attr('_href');
             var title = $(this).find('cite').html();
-            var index  = $('.left-nav #nav li').index($(this));
+            var index = $('.left-nav #nav li').index($(this));
 
-            for (var i = 0; i <$('.x-iframe').length; i++) {
-                if($('.x-iframe').eq(i).attr('tab-id')==index+1){
-                    tab.tabChange(index+1);
+            for (var i = 0; i < $('.x-iframe').length; i++) {
+                if ($('.x-iframe').eq(i).attr('tab-id') == index + 1) {
+                    tab.tabChange(index + 1);
                     event.stopPropagation();
                     return;
                 }
-            };
-            
-            tab.tabAdd(title,url,index+1);
-            tab.tabChange(index+1);
+            }
+            ;
+
+            tab.tabAdd(title, url, index + 1);
+            tab.tabChange(index + 1);
         }
 
         event.stopPropagation();
-         
+
     });
 
 
@@ -138,31 +140,74 @@ $(function () {
 
 /*弹出层*/
 /*
-    参数解释：
-    title   标题
-    url     请求的url
-    id      需要操作的数据id
-    w       弹出层宽度（缺省调默认值）
-    h       弹出层高度（缺省调默认值）
-*/
-function x_admin_show(title,url,w,h){
+ 参数解释：
+ title   标题
+ url     请求的url
+ id      需要操作的数据id
+ w       弹出层宽度（缺省调默认值）
+ h       弹出层高度（缺省调默认值）
+ */
+function x_admin_show(title, url, w, h) {
     if (title == null || title == '') {
-        title=false;
-    };
+        title = false;
+    }
+    ;
     if (url == null || url == '') {
-        url="404.html";
-    };
+        url = "404.html";
+    }
+    ;
     if (w == null || w == '') {
-        w=($(window).width()*0.9);
-    };
+        w = ($(window).width() * 0.9);
+    }
+    ;
     if (h == null || h == '') {
-        h=($(window).height() - 50);
-    };
+        h = ($(window).height() - 50);
+    }
+    ;
     layer.open({
         type: 2,
-        area: [w+'px', h +'px'],
+        area: [w + 'px', h + 'px'],
         fix: false, //不固定
         maxmin: true,
+        shadeClose: true,
+        shade: 0.4,
+        title: title,
+        content: url
+    });
+}
+
+/*弹出层*/
+/*
+ 参数解释：
+ title   标题
+ url     请求的url
+ id      需要操作的数据id
+ w       弹出层宽度（缺省调默认值）
+ h       弹出层高度（缺省调默认值）
+ */
+function x_admin_show_parent(title, url, w, h) {
+    if (title == null || title == '') {
+        title = false;
+    }
+    ;
+    if (url == null || url == '') {
+        url = "404.html";
+    }
+    ;
+    if (w == null || w == '') {
+        w = ($(window).width() * 0.9);
+    }
+    ;
+    if (h == null || h == '') {
+        h = ($(window).height() );
+    }
+    ;
+    parent.layer.open({
+        type: 2,
+        area: [w + 'px', h + 'px'],
+        fixed: false, //不固定
+        maxmin: true,
+        anim: 0,//0-6的动画形式，-1不开启
         shadeClose: true,
         shade:0.4,
         title: title,
@@ -171,7 +216,7 @@ function x_admin_show(title,url,w,h){
 }
 
 /*关闭弹出框口*/
-function x_admin_close(){
+function x_admin_close() {
     var index = parent.layer.getFrameIndex(window.name);
     parent.layer.close(index);
 }
