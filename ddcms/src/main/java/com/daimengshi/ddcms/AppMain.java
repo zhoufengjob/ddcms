2ebd820a8aba2f6a8f109a68a023ec83354cd8b9
周峰 <zhoufengjob@sina.com>
2017-12-27 14:05:40 +0800

    实现置顶排序

--- ddcms/src/main/java/com/daimengshi/ddcms/AppMain.java
+++ ddcms/src/main/java/com/daimengshi/ddcms/AppMain.java
@@ -5,11 +5,6 @@ import com.xiaoleilu.hutool.log.LogFactory;
 import io.jboot.Jboot;
 import io.jboot.server.JbootServer;
 import io.jboot.server.listener.JbootAppListenerBase;
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.config.IniSecurityManagerFactory;
-import org.apache.shiro.mgt.RealmSecurityManager;
-import org.apache.shiro.mgt.SecurityManager;
-import org.apache.shiro.util.Factory;
 
 /**
  * Created by zhoufeng on 2017/12/14.
@@ -27,7 +22,7 @@ public class AppMain  extends JbootAppListenerBase{
     public void onJbootStarted() {
         super.onJbootStarted();
         //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
-        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+//        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
 
 
         log.info("程序启动成功!");
